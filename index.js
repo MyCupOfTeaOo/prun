2066272004a53ebd5a7eeaf3bad1fbe59ed8eecf
zou wendi <542095059@qq.com>
2020-06-01 16:59:19 +0800

    feat: 增加向上查找路径

--- index.js
+++ index.js
@@ -4,28 +4,29 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
 const child_process = require("child_process");
+const pkgDir = require("pkg-dir");
 require("colors");
 
 const inputPath =
-  process.argv.slice(2, process.argv.length).find(arg => /[\/.]/.test(arg)) ||
+  process.argv.slice(2, process.argv.length).find((arg) => /[\/.]/.test(arg)) ||
   "./";
 const cwd = path.resolve(inputPath);
-const jsonPath = `${cwd}/package.json`;
 
 function gen_space(str) {
   return Array(str.length < 20 ? 24 - str.length : 4).join(" ") + " ";
 }
 
 function main() {
-  const has = fs.existsSync(jsonPath);
-  if (!has) {
+  const PROJECT_PATH = pkgDir.sync(path.resolve());
+  if (!PROJECT_PATH) {
     console.error(
-      "没有在该路径下找到 package.json 文件,路径:".red,
-      jsonPath.red
+      "没有在该路径及向上查找到 package.json 文件,路径:".red,
+      cwd.red
     );
     return;
   }
-  const json = JSON.parse(fs.readFileSync(jsonPath));
+  const JSON_PATH = path.resolve(PROJECT_PATH, "/package.json");
+  const json = JSON.parse(fs.readFileSync(JSON_PATH));
   const scripts = json.scripts;
   if (!scripts) {
     console.error("package.json里没有scripts脚本".red);
@@ -43,18 +44,18 @@ function main() {
         type: "list",
         name: "script",
         message: "What do you want to run script?",
-        choices: keys.map(script => ({
+        choices: keys.map((script) => ({
           name: `${script}${gen_space(script)}${scripts[script]}`,
-          value: script
-        }))
-      }
+          value: script,
+        })),
+      },
     ])
-    .then(answers => {
+    .then((answers) => {
       console.log(answers);
       child_process.spawn(
         process.platform === "win32" ? "npm.cmd" : "npm",
         ["run", answers.script],
-        { cwd, stdio: "inherit" }
+        { cwd: PROJECT_PATH, stdio: "inherit" }
       );
     });
 }
